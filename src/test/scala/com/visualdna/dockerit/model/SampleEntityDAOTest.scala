7b0b30cc570c859cc432df6259b907277523e202
Paolo Rascuna <paolo.rascuna@visualdna.com>
2015-12-01 12:36:23 +0000

    runwith junit

--- src/test/scala/com/visualdna/dockerit/model/SampleEntityDAOTest.scala
+++ src/test/scala/com/visualdna/dockerit/model/SampleEntityDAOTest.scala
@@ -1,8 +1,11 @@
 package com.visualdna.dockerit.model
 
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.{Matchers, path, FunSuite}
 
+@RunWith(classOf[JUnitRunner])
 class SampleEntityDAOTest extends path.FunSpec with Matchers with MockitoSugar {
 
 }
