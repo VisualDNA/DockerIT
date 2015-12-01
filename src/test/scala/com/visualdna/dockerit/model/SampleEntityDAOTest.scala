d189e34fdeca12102b0f295ec9822915b28cf10c
Paolo Rascuna <paolo.rascuna@visualdna.com>
2015-12-01 13:03:29 +0000

    mysql image configured

--- src/test/scala/com/visualdna/dockerit/model/SampleEntityDAOTest.scala
+++ src/test/scala/com/visualdna/dockerit/model/SampleEntityDAOTest.scala
@@ -1,11 +1,25 @@
 package com.visualdna.dockerit.model
 
+import java.util.Properties
+
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.mock.MockitoSugar
-import org.scalatest.{Matchers, path, FunSuite}
+import org.scalatest.{Matchers, path}
+import slick.driver.MySQLDriver.api._
 
 @RunWith(classOf[JUnitRunner])
-class SampleEntityDAOTest extends path.FunSpec with Matchers with MockitoSugar {
+class SampleEntityDAOTest extends path.FunSpec with Matchers with MockitoSugar with MySqlTest {
+  
+  val db = getTestDb(new Properties())
+
+  describe("SampleEntityDAO") {
+    DBIO.seq(
+    )
+
+    val dao = new SampleEntityDAO(db)
+    it("should be able to connect to the database") {
+    }
+  }
 
 }
