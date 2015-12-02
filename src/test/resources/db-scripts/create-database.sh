0aa7df9080fe1b5d279d826a59453bed17c2935a
Cristian Arcaroli <cristian.arcaroli@imagini.net>
2015-12-02 14:11:35 +0000

    Incremented starting polling interval

--- src/test/resources/db-scripts/create-database.sh
+++ src/test/resources/db-scripts/create-database.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while ! echo $(mysql -h 127.0.0.1 -P 3306 -u${test.mysql.root.username} -p${test.mysql.root.password} -e "SELECT 'ready'") | grep --quiet "ready"; do
-    sleep 1
+    sleep 2
     echo "====================================="
     echo "Waiting for mysql Server to be ready"
     echo "====================================="
