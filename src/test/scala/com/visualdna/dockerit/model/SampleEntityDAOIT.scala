d5f8f4fdbd338de9fdef45957ca88a249093875a
Vladimirs Matusevics <vladimir.matusevic@imagini.net>
2015-12-02 12:21:21 +0000

    test

--- src/test/scala/com/visualdna/dockerit/model/SampleEntityDAOIT.scala
+++ src/test/scala/com/visualdna/dockerit/model/SampleEntityDAOIT.scala
@@ -26,7 +26,7 @@ class SampleEntityDAOIT extends FunSpec with Matchers with MockitoSugar with MyS
       val res = for {
         _ <- dao.save("javascript", "rocks")
         _ <- dao.save("javascript", "I told you it rocks")
-        _ <- dao.save("java", "Mmmm... Seriously?")
+        _ <- dao.save("java", "Hmmm... Seriously?")
       } yield {}
 
       val futureResult: Future[Seq[SampleEntity]] = res.flatMap( _ => dao.read("javascript"))
