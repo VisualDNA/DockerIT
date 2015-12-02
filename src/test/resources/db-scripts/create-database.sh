b8c28e0d51011b75205431a39b846c1511404d01
Cristian Arcaroli <cristian.arcaroli@imagini.net>
2015-12-02 14:09:33 +0000

    Added logging to starting script

--- src/test/resources/db-scripts/create-database.sh
+++ src/test/resources/db-scripts/create-database.sh
@@ -2,7 +2,10 @@
 
 while ! echo $(mysql -h 127.0.0.1 -P 3306 -u${test.mysql.root.username} -p${test.mysql.root.password} -e "SELECT 'ready'") | grep --quiet "ready"; do
     sleep 1
-    echo "testing"
+    echo "====================================="
+    echo "Waiting for mysql Server to be ready"
+    echo "====================================="
+
 done
 
 mysql -h 127.0.0.1 -P 3306 -u${test.mysql.root.username} -p${test.mysql.root.password} < $1
